50b6033dbd3dd22e3ff4393566e9aefaebdab703
ValentineFlo <vflorens@gaming.tech>
2025-01-27 22:20:04 +0100

    Ajoutez des fichiers projet.

---
+++ Template_LibraryMaths/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "List.h"
+
+#include <iostream>
+
+int main()
+{
+    List<size_t> toto;
+    toto.push_back(12);
+    std::cout << toto.back() << std::endl;
+    toto.push_back(42);
+    std::cout << toto.back() << std::endl;
+    toto.pop_back();
+    std::cout << toto.size() << std::endl;
+    std::cout << toto.back() << std::endl;
+    toto.pop_back();
+    std::cout << toto.size() << std::endl;
+
+    std::cout << "\n\n";
+
+    toto.push_front(36);
+    toto.push_front(24);
+    std::cout << toto.size() << std::endl;
+    std::cout << toto.front() << std::endl;
+    toto.pop_front();
+    std::cout << toto.size() << std::endl;
+    std::cout << toto.front() << std::endl;
+    toto.pop_front();
+    std::cout << toto.size() << std::endl;
+}
